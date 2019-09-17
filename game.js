09e249474f71e28c2ee154705bfa00f6084c6eaf
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-09-17 13:33:53 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,11 +4,16 @@ postToPage(score); // Print to the page
 
 createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
+createButton('Button', botnPress);
 
 setInterval(btnPress, 1000);
+setInterval(botnPress, -1000);
 
 function btnPress() {
   score++;
   postToPage(score);
 }
-
+function botnPress() {
+  score--;
+  postToPage(score);
+}
