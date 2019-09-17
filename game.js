b475b5c07925308011dc6eda216e01694795f285
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-09-17 14:12:06 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,15 +3,18 @@ var score = 0;
 postToPage(score); // Print to the page
 
 createButton('Button', btnPress); // Make a button
-changeTitle('Clicker Game');
-createButton('Button', botnPress);
+changeTitle('What does dis do?');
+createButton('Bad Button', botnPress);
 
 setInterval(btnPress, 1000);
-setInterval(botnPress, -1000);
+setInterval(botnPress, 1000);
 
 function btnPress() {
   score++;
   postToPage(score);
+  if(score = 10) {
+  changeTitle('You win!') 
+ }
 }
 function botnPress() {
   score--;
