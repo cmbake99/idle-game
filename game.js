33f2e14a4991cd3b9ac64e56eb8356f6b4046a15
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-09-18 14:31:40 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
 var score = 1; // Create a variable to store the score in
 
+
 var title = new Title('Welcome to the Game'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
+var hidden = new Button('Boo!', botnPress);
+var button = new Button('memes', bootnPress);
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
+
 
 function btnPress() {
   score++;         // Increase the score
@@ -15,6 +18,26 @@ function btnPress() {
     hidden.show();
   }
   if(score > 15) {
+    hidden.hide(500);
+  }
+}
+function botnPress() {
+  score--;         // Increase the score
+  scr.edit(score); // Update the page with the new score
+  if(score > 10) {
+    hidden.show();
+  }
+  if(score > 100) {
+    hidden.hide(500);
+  }
+}
+function bootnPress() {
+  score++;         // Increase the score
+  scr.edit(score); // Update the page with the new score
+  if(score > 10) {
+    hidden.show();
+  }
+  if(score > 100) {
     hidden.hide(500);
   }
 }
