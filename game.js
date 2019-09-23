4c78b47386bb2a8c8f8d61187ed530a2dfdcbc2d
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-09-23 14:30:55 -0400

    h

--- game.js
+++ game.js
@@ -3,8 +3,8 @@ var score = 1; // Create a variable to store the score in
 
 var title = new Title('Hero Risen'); // Create a title element
 var button = new Button('Gather Wood', btnPress);
-var button = new Button('memes', bootnPress);  // Create a button element
-var hidden = new Button('Boo!', botnPress);
+var hidden = new Button('Gather Stone', botnPress);  // Create a button element
+
 
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
@@ -15,22 +15,18 @@ setInterval(btnPress, 1000);   // Set up a loop
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 100) {
-    + botnPress()
+  if(score < 100) {
+    hidden.show();
   }
-  if(score > 101) {
-    hidden.hide(500);
-  }
+
 }
 function botnPress() {
-  score--;         // Increase the score
+  score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 10) {
-    var button = new Button('Build a House', btnPress);
-  }
   if(score > 100) {
-    hidden.hide(500);
+    var button = new Button('Gather Iron', bootnPress);
   }
+
 }
 function bootnPress() {
   score++;         // Increase the score
@@ -38,10 +34,5 @@ function bootnPress() {
   if(score > 10) {
     hidden.show();
   }
-  if(score > 100) {
-    hidden.hide(500);
-  }
-}
-function booHide() {
-  hidden.hide(1)
+
 }
