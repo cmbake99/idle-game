644872d139ef6a30e11d0d619ace8fa54e193650
Chase baker <cmbake99@harrisonburg.k12.va.us>
2019-09-26 13:35:13 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,23 @@
 var score = 1; // Create a variable to store the score in
 var score2 = 1;
+var score3 = 1;
+var score4 = 1;
 
 var title = new Title('Hero Risen'); // Create a title element
 var button = new Button('Gather Wood', btnPress);
 var button2 = new Button('Gather Stone', botnPress);
 button2.hide(); // Create a button element
+var button3 = new Button('Gather Coal', bootnPress);
+button3.hide();
+var button4 = new Button('Gather Iron', boootnPress);
+button4.hide();
 var emptyText = new Text('');
+var emptyText2 = new Text('');
+var emptyText3 = new Text('');
 var scr = new Text(score);
-var scr2 = new Text(score2);                    // Create a text element
+var scr2 = new Text(score2);
+var scr3 = new Text(score3);
+var scr4 = new Text(score4);
 
 setInterval(btnPress, 1000);   // Set up a loop
 
@@ -17,14 +27,30 @@ function btnPress() {
   scr.edit(score); // Update the page with the new score
   if(score > 100) {
     button2.show();
-    setInterval(botnPress, 10000);
   }
 }
 function botnPress() {
   score2++;         // Increase the score
   scr2.edit(score2); // Update the page with the new score
-  if(score > 100) {
-    button2.show();
+  if(score2 > 100) {
+    button3.show();
+
+  }
+}
+function bootnPress() {
+  score3++;         // Increase the score
+  scr3.edit(score3); // Update the page with the new score
+  if(score3 > 100) {
+    button4.show();
+    //setInterval(bootnPress, 10000);
+  }
+}
+function boootnPress() {
+  score4++;         // Increase the score
+  scr4.edit(score4); // Update the page with the new score
+  if(score4 > 100) {
+    button5.show();
+    //setInterval(bootnPress, 10000);
   }
 }
 
@@ -41,7 +67,13 @@ rowSection.add(column2);  // Add the second column to the row
 column1.add(text1);
 column1.add(button);
 column1.add(emptyText);
-column1.add(button2); // Add the text to the first column
+column1.add(button2);
+column1.add(emptyText2);
+column1.add(button3);
+column1.add(emptyText3);
+column1.add(button4);
 column2.add(text2);
-column2.add(scr); // Add the text to the second column
+column2.add(scr);
 column2.add(scr2);
+column2.add(scr3);
+column2.add(scr4);
